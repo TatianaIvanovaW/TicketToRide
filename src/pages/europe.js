1559e1047d1d7fb58a1f7941862028a01071b6e0
Tatiana Ivanova <ivanova.tatiana.w@gmail.com>
2020-12-12 01:14:37 +0100

    add routes and stations - checkboxes work only ones

--- src/pages/europe.js
+++ src/pages/europe.js
@@ -1,12 +1,11 @@
 import React from "react";
-// import { useState } from "react";
 import Header from "../components/Header";
 import { Form, Row, Col, Button, ButtonGroup } from "react-bootstrap";
 import { useState } from "react";
 import longRoute from "../data/long";
+import shortRoute from "../data/short";
 
 export default function Europe() {
-  // const [score, set_score] = useState;
   const img = "https://img.icons8.com/android/24/000000/railroad-car.png";
   const [value1, set_value1] = useState(0);
   const [value2, set_value2] = useState(0);
@@ -17,19 +16,27 @@ export default function Europe() {
   const [name, set_name] = useState("");
   const [screenName, set_screenName] = useState("");
   const [finalScore, set_finalScore] = useState(0);
+  const [longRouteScore, set_longRouteScore] = useState(0);
+  const [stations, set_stations] = useState(0);
 
-  console.log(longRoute);
+  const shortRouteSorted = shortRoute.sort((a, b) => {
+    return a.name.localeCompare(b.name);
+  });
+  // console.log(longRoute);
+  // console.log(shortRoute.length);
 
-  const data = [
+  let data = [
     value1,
     value2 * 2,
     value3 * 4,
     value4 * 7,
     value6 * 15,
     value8 * 21,
+    parseInt(longRouteScore),
+    stations,
   ];
 
-  console.log(name);
+  console.log(`data array`, data);
 
   const reset = () => {
     set_value1(0);
@@ -40,9 +47,16 @@ export default function Europe() {
     set_value8(0);
     set_finalScore(0);
     set_screenName("");
+    set_longRouteScore(0);
   };
 
   const countScore = (e) => {
+    const sunShort = dataShort.reduce((sum, num) => {
+      return sum + num;
+    });
+    console.log(sunShort);
+    data.push(sunShort);
+    console.log(`hew`, data);
     const score = data.reduce((sum, num) => {
       return sum + num;
     });
@@ -50,6 +64,11 @@ export default function Europe() {
     e.preventDefault();
     set_screenName(name);
     set_finalScore(score);
+  };
+  let dataShort = [];
+  const checkbox = (e) => {
+    dataShort.push(parseInt(e.target.value));
+    console.log(dataShort);
   };
 
   return (
@@ -77,7 +96,6 @@ export default function Europe() {
             Score : {finalScore}
           </Form.Label>
         </Form.Group>
-
         <fieldset>
           <Form.Group as={Row}>
             <Form.Label as="legend" column sm={2}>
@@ -132,6 +150,55 @@ export default function Europe() {
           </Form.Group>
         </fieldset>
 
+        <Form.Group controlId="exampleForm.ControlSelect1">
+          <Form.Label>Your long route:</Form.Label>
+          <Form.Control
+            onChange={(e) => {
+              set_longRouteScore(e.target.value);
+            }}
+            as="select"
+          >
+            <option value={0}>choose a route...</option>
+            {longRoute.map((route) => {
+              return (
+                <option key={route.name} value={route.score}>
+                  {route.name}
+                </option>
+              );
+            })}
+          </Form.Control>
+        </Form.Group>
+        <Form.Group controlId="exampleForm.ControlSelect1">
+          <Form.Label>Stations used:</Form.Label>
+          <Form.Control
+            onChange={(e) => {
+              set_stations(parseInt(e.target.value));
+            }}
+            as="select"
+          >
+            <option value={12}>0</option>
+            <option value={8}>1</option> <option value={4}>2</option>
+            <option value={0}>3</option>
+          </Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Your short routes: </Form.Label>
+          <div key={`inline-checkbox`} className="mb-3">
+            {shortRouteSorted.map((route) => {
+              return (
+                <Form.Check
+                  onChange={checkbox}
+                  key={route.name}
+                  inline
+                  value={route.score}
+                  label={route.name}
+                  type="checkbox"
+                  id={`inline-checkbox-2`}
+                />
+              );
+            })}
+          </div>
+        </Form.Group>
         <Form.Group as={Row}>
           <Col sm={{ span: 10, offset: 2 }}>
             <Button onClick={countScore} type="submit">
